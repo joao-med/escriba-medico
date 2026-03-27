ba8f691881a826cbc0b8d6675501b0e09eaeb775
Joao Pedro <72229642+joao-med@users.noreply.github.com>
2026-03-26 23:23:57 -0300

    feat: escriba medico fase 0

---
+++ prompts/__init__.py
@@ -0,0 +1,4 @@
+from .agent1_translator import TRANSLATOR_PROMPT
+from .agent2_evaluator import EVALUATOR_PROMPT
+from .agent3_emergency import EMERGENCY_PROMPT
+from .agent4_summarizer import SUMMARIZER_PROMPT, REVISION_PROMPT
